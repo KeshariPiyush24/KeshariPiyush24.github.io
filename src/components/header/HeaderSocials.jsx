ad0f87a1ff6f1e6e880235755bb36f1a1fc89c51
Piyush Keshari <piyushkeshari24@gmail.com>
2024-10-28 19:33:13 +0530

    updated header connect icons

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -1,22 +1,27 @@
 import React from 'react'
-import { BsLinkedin, BsGithub, BsTwitter } from 'react-icons/bs'
+import { BsTwitter, BsInstagram, BsGithub, BsLinkedin } from 'react-icons/bs'
 
-function HeaderSocials() {
+const HeaderSocials = () => {
   return (
     <div className="header_socials">
-      <a href="https://www.linkedin.com/in/piyush-k-ba3a67134/" target="_blank" rel="noreferrer">
-        <div className="circle linkedin">
-          <BsLinkedin className="header_social_icon" />
+      <a href="https://twitter.com/KeshariPiyush24" target="_blank" rel="noreferrer">
+        <div className="circle twitter">
+          <BsTwitter className='header_social_icon' />
+        </div>
+      </a>
+      <a href="https://www.instagram.com/kesharipiyush24/" target="_blank" rel="noreferrer">
+        <div className="circle instagram">
+          <BsInstagram className='header_social_icon' />
         </div>
       </a>
       <a href="https://github.com/KeshariPiyush24" target="_blank" rel="noreferrer">
         <div className="circle github">
-          <BsGithub className="header_social_icon" />
+          <BsGithub className='header_social_icon' />
         </div>
       </a>
-      <a href="https://twitter.com/KeshariPiyush24" target="_blank" rel="noreferrer">
-        <div className="circle twitter">
-          <BsTwitter className="header_social_icon" />
+      <a href="https://www.linkedin.com/in/piyush-k-ba3a67134/" target="_blank" rel="noreferrer">
+        <div className="circle linkedin">
+          <BsLinkedin className='header_social_icon' />
         </div>
       </a>
     </div>
