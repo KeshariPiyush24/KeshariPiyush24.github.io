89e0f65b06aa79310e37082457c0309d377d0e23
Piyush Keshari <piyushkeshari24@gmail.com>
2024-12-06 11:38:37 +0530

    added sponsor button

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
 import cv from '../../assets/cv.pdf'
+import { AiFillHeart } from 'react-icons/ai'
+import './cta.css'
 
 function CTA() {
   return (
     <div className="cta">
       <a href={cv} download="Piyush's Resume.pdf" className='btn'>Download CV</a>
       <a href="#contact" className="btn btn-primary">Let's Talk</a>
+      <a
+        href="https://github.com/sponsors/KeshariPiyush24"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="btn btn-sponsor"
+      >
+        <AiFillHeart className="sponsor-heart" /> Sponsor
+      </a>
     </div>
   )
 }
